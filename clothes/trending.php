14bfb2acd556cba97aa7b91aa0f9ba172a39a1fb
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-20 23:37:18 +0300

    improvements have been made

--- clothes/trending.php
+++ clothes/trending.php
@@ -1,23 +1,28 @@
 <?php
 include '../connection.php';
-$minRating = 4.4;
-$limitClothesItem = 5;
 
-$sqlQuery = "Select *FROM items_table WHERE rating>='$minRating' ORDER BY rating DESC LIMIT $limitClothItems";
-//newly available top rated clothes item=5
+
+$minRating = 3;
+$limitClothItems = 5;
+
+$sqlQuery = "Select * FROM items_table WHERE rating>= '$minRating' ORDER BY rating DESC LIMIT $limitClothItems";
+//5 or less than 5 newly available top rated clothes item
+//not greater than 5
+
 $resultOfQuery = $connectNow->query($sqlQuery);
+
 if ($resultOfQuery->num_rows > 0) {
     $clothItemsRecord = array();
     while ($rowFound = $resultOfQuery->fetch_assoc()) {
         $clothItemsRecord[] = $rowFound;
     }
+
     echo json_encode(
         array(
             "success" => true,
-            "clothesItemsData" => $clothItemsRecord[0],
+            "clothItemsData" => $clothItemsRecord,
         )
     );
-
 } else {
     echo json_encode(array("success" => false));
 }
