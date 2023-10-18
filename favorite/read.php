08685ac851ce85bd1d704a89b11ee22292e3260e
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-10-18 17:14:03 +0300

    update

--- favorite/read.php
+++ favorite/read.php
@@ -2,7 +2,7 @@
 include '../connection.php';
 $currentOnlineUserID = $_POST["user_id"];
 
-$sqlQuery = "SELECT *FROM favorite_table CROSS JOIN items_table where favorite_table.user_id='currentOnlineUserID' and favorite_table.item_id=items_table.item_id";
+$sqlQuery = "SELECT *FROM favorite_table CROSS JOIN items_table where favorite_table.user_id='$currentOnlineUserID' AND favorite_table.item_id=items_table.item_id";
 $resultOfQuery = $connectNow->query($sqlQuery);
 if ($resultOfQuery->num_rows > 0) {
     $favoriteRecord = array();
@@ -12,11 +12,10 @@ if ($resultOfQuery->num_rows > 0) {
     echo json_encode(
         array(
             "success" => true,
-
             "currentUserFavoriteData" => $favoriteRecord
         )
     );
 } else {
-    echo json_encode(array("success" => false))
-    ;
+    echo json_encode(array("success" => false));
+
 }
