17865c6d7902239ed223b217d9fedb128bb531e3
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-10-18 18:13:32 +0300

    query and if else added

---
+++ items/search.php
@@ -0,0 +1,19 @@
+<?php
+include '../connection.php';
+
+
+$typedKeywords=$_POST['typedKeywords'];
+
+$sqlQuery="SELECT *from items_table where name like '$typedKeywords'";
+
+$resultOfQuery=$connectNow->query($sqlQuery);
+if($resultOfQuery->num_rows>0){
+    $foundItemsRecord=array();
+    while($rowFound=$resultOfQuery->fetch_assoc()){
+$foundItemsRecord[]=$rowFound;
+    }
+    echo json_encode(array("success"=>true,"itemFoundData"=>$foundItemsRecord));
+}
+else{
+    echo json_encode(array("success"=>false));
+}
