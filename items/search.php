000931164ed839bb88bd388c83ad6da42426231b
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-10-20 17:06:52 +0300

    search content edited

--- items/search.php
+++ items/search.php
@@ -2,18 +2,17 @@
 include '../connection.php';
 
 
-$typedKeywords=$_POST['typedKeywords'];
+$typedKeywords = $_POST['typedKeywords'];
 
-$sqlQuery="SELECT *from items_table where name like '$typedKeywords'";
+$sqlQuery = "SELECT *from items_table where name like '$typedKeywords'";
 
-$resultOfQuery=$connectNow->query($sqlQuery);
-if($resultOfQuery->num_rows>0){
-    $foundItemsRecord=array();
-    while($rowFound=$resultOfQuery->fetch_assoc()){
-$foundItemsRecord[]=$rowFound;
+$resultOfQuery = $connectNow->query($sqlQuery);
+if ($resultOfQuery->num_rows > 0) {
+    $foundItemsRecord = array();
+    while ($rowFound = $resultOfQuery->fetch_assoc()) {
+        $foundItemsRecord[] = $rowFound;
     }
-    echo json_encode(array("success"=>true,"itemsFoundData"=>$foundItemsRecord));
-}
-else{
-    echo json_encode(array("success"=>false));
+    echo json_encode(array("success" => true, "itemsFoundData" => $foundItemsRecord));
+} else {
+    echo json_encode(array("success" => false));
 }
