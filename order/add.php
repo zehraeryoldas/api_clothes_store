3d87b8a356a62a8d06d801985c2cb08c1215de08
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-12-01 23:30:26 +0300

    small changes

--- order/add.php
+++ order/add.php
@@ -1,24 +1,33 @@
 <?php
 include '../connection.php';
 
-$userID=$_POST["user_id"];
-$selectedItems=$_POST["selectedItems"];
-$deliverSystem=$_POST["deliverSystem"];
-$paymentSystem=$_POST["paymentSystem"];
-$note=$_POST["note"];
-$totalamount=$_POST["totalamount"];
-$image=$_POST["image"];
-$status=$_POST["status"];
-$dateTime=$_POST["dateTime"];
-$shipmentAddress=$_POST["shipmentAddress"];
-$phoneNumber=$_POST["phoneNumber"];
-$imageFileBase64=$_POST["imageFile"];
+//verdiğim siparişi veritabanına kaydediyorum
+$userID = $_POST["user_id"];
+$selectedItems = $_POST["selectedItems"];
+$deliverySystem = $_POST["deliverySystem"];
+$paymentSystem = $_POST["paymentSystem"];
+$note = $_POST["note"];
+$totalAmount = $_POST["totalAmount"];
+$image = $_POST["image"];
+$status = $_POST["status"];
+$shipmentAddress = $_POST["shipmentAddress"];
+$phoneNumber = $_POST["phoneNumber"];
+$imageFileBase64 = $_POST["imageFile"];
 
 
-$sqlQuery="INSERT INTO orders_table SET user_id='$userID',selectedItems='$selectedItems',deliverSystem='$deliverSystem',paymentSystem='$paymentSystem',note='$note',totalamount='$totalamount',image='$image',status='$status',dateTime='$dateTime',shipmentAddress='$shipmentAddress',phoneNumber='$phoneNumber',imageFile='$imageFileBase64'";
-if($resultOfQuery){
-  
-    echo json_encode(array("success" => true, "itemsFoundData" => $foundItemsRecord));
-}else {
-    echo json_encode(array("success" => false));
+$sqlQuery = "INSERT INTO orders_table SET user_id='$userID', selectedItems='$selectedItems', deliverySystem='$deliverySystem', paymentSystem='$paymentSystem', note='$note', totalAmount='$totalAmount', image='$image', status='$status', shipmentAddress='$shipmentAddress', phoneNumber='$phoneNumber'";
+
+$resultOfQuery = $connectNow->query($sqlQuery);
+
+if($resultOfQuery)
+{
+    //upload image to server
+    $imageFileOfTransactionProof = base64_decode($imageFileBase64);
+    file_put_contents("order/transactions_proof_images/".$image, $imageFileOfTransactionProof);
+
+    echo json_encode(array("success"=>true));
+}
+else
+{
+    echo json_encode(array("success"=>false));
 }
